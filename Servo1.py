3155c59da3c45d5263110261a5968ee40b232ebf
Zhi29 <zhili5494@gmail.com>
2019-03-05 18:45:46 -0800

    pWM test

--- Servo1.py
+++ Servo1.py
@@ -384,10 +384,10 @@ def motor_mix_controller(u1, u2):
     # the Force vector is force for each motor.
     Force = np.dot(Motor_mix, np.array([u1,u2[0],u2[1],u2[2]]))
     Force = np.maximum(Force, m*g/10)
-    #print("u1: ", u1)
-    #print("u2: ", u2)
+    print("u1: ", u1)
+    print("u2: ", u2)
     #print("Motor_mix: ", Motor_mix)
-    #print("Force: ", Force)
+    print("Force: ", Force)
 
     # transform force of each motor into rotation speed :
     omega = np.sqrt(1/k * Force)
