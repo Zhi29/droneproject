2030967a52f9257588e71fb2e1c555d48581999f
Zhi29 <zhili5494@gmail.com>
2019-03-05 17:53:30 -0800

    pWM test

--- Servo1.py
+++ Servo1.py
@@ -402,10 +402,10 @@ def motor_mix_controller(u1, u2):
 
 def drive_motor(control_PWM):
     # this function is mainly used to pass duty cycle into navio hardware to drive motor.
-    loop_for(0.01, pwm0.set_duty_cycle, control_PWM[0])
-    loop_for(0.01, pwm1.set_duty_cycle, control_PWM[1])
-    loop_for(0.01, pwm2.set_duty_cycle, control_PWM[2])
-    loop_for(0.01, pwm3.set_duty_cycle, control_PWM[3])
+    loop_for(0.001, pwm0.set_duty_cycle, control_PWM[0])
+    loop_for(0.001, pwm1.set_duty_cycle, control_PWM[1])
+    loop_for(0.001, pwm2.set_duty_cycle, control_PWM[2])
+    loop_for(0.001, pwm3.set_duty_cycle, control_PWM[3])
 
 def calculate_position(c, t):
     """
