b983e949732a60320356ea5350b8c505af2fab45
Zhi29 <zhili5494@gmail.com>
2019-03-07 08:22:40 -0800

    testPWM

--- Servo1.py
+++ Servo1.py
@@ -167,10 +167,10 @@ def test_throttle_via_RC():
 
 def wait_until_motor_is_ready():
     print("I am in the wait_until_motor_is_ready()")
-    loop_for(0.1, pwm0.set_duty_cycle, SERVO_MIN)
-    loop_for(0.1, pwm1.set_duty_cycle, SERVO_MIN)
-    loop_for(0.1, pwm2.set_duty_cycle, SERVO_MIN)
-    loop_for(0.1, pwm3.set_duty_cycle, SERVO_MIN)
+    loop_for(0.01, pwm0.set_duty_cycle, SERVO_MIN)
+    loop_for(0.01, pwm1.set_duty_cycle, SERVO_MIN)
+    loop_for(0.01, pwm2.set_duty_cycle, SERVO_MIN)
+    loop_for(0.01, pwm3.set_duty_cycle, SERVO_MIN)
 
     time.sleep(8)
 
