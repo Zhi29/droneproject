c4afbd998693897d4e4da439e6389446c00f6766
Zhi29 <zhili5494@gmail.com>
2019-03-07 07:37:17 -0800

    testPWM

--- Servo1.py
+++ Servo1.py
@@ -570,7 +570,7 @@ def main():
     Calculates the x, y, z coefficients for the four segments 
     of the trajectory
     """
-    #wait_until_motor_is_ready()
+    wait_until_motor_is_ready()
 
     pos, _, _, _ = reading_positional_info()
     x_start = pos[0]
