be139a9259a1b5e04a613165a399abd88b682bc9
Zhi29 <zhili5494@gmail.com>
2019-03-10 10:48:22 -0700

    test motor

--- Servo1.py
+++ Servo1.py
@@ -585,7 +585,7 @@ def main():
     """
     #calibration_ESC()
     wait_until_motor_is_ready()
-    loop_for(0.1, pwm0.set_duty_cycle, SERVO_MIN)
+    loop_for(1, pwm0.set_duty_cycle, SERVO_MIN)
     loop_for(0.1, pwm1.set_duty_cycle, SERVO_MIN)
     loop_for(0.1, pwm2.set_duty_cycle, SERVO_MIN)
     loop_for(0.1, pwm3.set_duty_cycle, SERVO_MIN)
