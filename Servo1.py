dfa7b8c2808d2518423b1b48ac651b5728d953e0
Zhi29 <zhili5494@gmail.com>
2019-03-10 11:19:51 -0700

    test motor

--- Servo1.py
+++ Servo1.py
@@ -305,8 +305,8 @@ Euler_error_store = []
 
 
 #PD control parameters#######################################
-K_p_roll = 5
-K_p_pitch = 5
+K_p_roll = 50
+K_p_pitch = 50
 K_p_yaw = 5
 
 K_d_roll = 0.5 #0.1
