2474c7900ee860e7e9faf5d28c1c7ba6c10b6784
Zhi29 <zhili5494@gmail.com>
2019-04-17 16:52:44 -0700

    reading

--- Servo1.py
+++ Servo1.py
@@ -485,10 +485,10 @@ def drive_motor(control_PWM):
         #control_PWM[k] = ("%.3f" % control_PWM[k])
         #control_PWM[k] = format(control_PWM[k], '.4g')
     #print(control_PWM)
-    loop_for(0.001, pwm0.set_duty_cycle, control_PWM[0])
-    loop_for(0.001, pwm1.set_duty_cycle, control_PWM[1])
-    loop_for(0.001, pwm2.set_duty_cycle, control_PWM[2])
-    loop_for(0.001, pwm3.set_duty_cycle, control_PWM[3])
+    loop_for(0.0005, pwm0.set_duty_cycle, control_PWM[0])
+    loop_for(0.0005, pwm1.set_duty_cycle, control_PWM[1])
+    loop_for(0.0005, pwm2.set_duty_cycle, control_PWM[2])
+    loop_for(0.0005, pwm3.set_duty_cycle, control_PWM[3])
 
 def calculate_position(c, t):
     """
