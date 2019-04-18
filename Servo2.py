e40e426724aaefdeef823ffa0ea0378f6942cb4e
Zhi29 <zhili5494@gmail.com>
2019-04-18 15:48:54 -0700

    new traj

--- Servo2.py
+++ Servo2.py
@@ -382,10 +382,12 @@ def position_control(desired_pos_info, pos, vel, loop): #input should be generat
 
 	desired_pose = np.array([desired_roll, desired_pitch, desired_yaw])
 
-	judge = (np.abs(pos_error) <= 0.02)
-	if judge[0] == True and judge[1] == True and judge[2] == True:
+	#judge = (np.abs(pos_error) <= 0.02)
+	#if judge[0] == True and judge[1] == True and judge[2] == True:
+	#	loop = False
+	judge = np.abs(pos_error[2]) <= 0.02
+	if judge == True:
 		loop = False
-
 
 
 
